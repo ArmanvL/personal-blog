9a8abed1a8fab8dad1d909835dc56d9d6e354973
Arman van Lieshout <arman.van.lieshout@cxcompany.com>
2020-11-07 18:03:04 +0100

    [WiP] Adds Google Analytics tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-Y3YV0SSFSZ`,
       },
     },
     `gatsby-plugin-feed`,
